f7e7655254ec463ae54ded5af6c4494aab2d3264
Louka2b <ldeplace@louka2b.fr>
2026-04-16 17:29:20 +0200

    add signal gestion

--- src/includes/prompt.h
+++ src/includes/prompt.h
@@ -6,17 +6,24 @@
 /*   By: louka <louka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 12:02:28 by louka             #+#    #+#             */
-/*   Updated: 2026/04/16 12:17:32 by louka            ###   ########.fr       */
+/*   Updated: 2026/04/16 14:42:02 by louka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROMPT_H
 # define PROMPT_H
 
+# include <signal.h>
+# include <unistd.h>
+# include <errno.h>
+# include <termios.h>
+# include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 
+extern volatile sig_atomic_t	g_signal;
+
 const char	*get_prompt(void);
-int			launch_minishell(void);
+int			shell_loop(void);
 
 #endif
--- src/prompt.c
+++ src/prompt.c
@@ -6,26 +6,67 @@
 /*   By: louka <louka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/16 12:09:53 by louka             #+#    #+#             */
-/*   Updated: 2026/04/16 12:33:22 by louka            ###   ########.fr       */
+/*   Updated: 2026/04/16 15:30:53 by louka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
+#include <errno.h>
+
+volatile sig_atomic_t	g_signal = 0;
+
+static void	on_sigint(int signum)
+{
+	(void)signum;
+	g_signal = SIGINT;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+}
+
+static void	setup_signals(void)
+{
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+	struct termios		term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	ft_bzero(&sa_int, sizeof(sa_int));
+	ft_bzero(&sa_quit, sizeof(sa_quit));
+	sa_int.sa_handler = on_sigint;
+	sigemptyset(&sa_int.sa_mask);
+	sa_quit.sa_handler = SIG_IGN;
+	sigemptyset(&sa_quit.sa_mask);
+	sigaction(SIGINT, &sa_int, NULL);
+	sigaction(SIGQUIT, &sa_quit, NULL);
+}
 
 const char	*get_prompt(void)
 {
 	return ("minishell> ");
 }
 
-int	launch_minishell(void)
+int	shell_loop(void)
 {
 	char	*line;
 
+	setup_signals();
 	while (1)
 	{
 		line = readline(get_prompt());
+		if (g_signal == SIGINT)
+		{
+			g_signal = 0;
+			free(line);
+			continue ;
+		}
 		if (!line)
+		{
+			ft_printf("\nexit");
 			break ;
+		}
 		if (line[0] != '\0')
 			add_history(line);
 		free(line);
@@ -33,4 +74,8 @@ int	launch_minishell(void)
 	return (0);
 }
 
-
+int	main(void)
+{
+	shell_loop();
+	return (0);
+}
