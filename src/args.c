8aa4bf90a5726fbda78f1ffa5a73548f4f02cb93
hchartie <chartierhugo098@gmail.com>
2026-04-25 14:58:50 +0200

    Init cmd array in args struct

--- src/args.c
+++ src/args.c
@@ -6,7 +6,7 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 15:23:11 by hchartie          #+#    #+#             */
-/*   Updated: 2026/04/25 04:59:45 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/04/25 15:02:40 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,16 @@ t_args	*parse_args(char **tokens)
 {
 	t_args	*args;
 
-	args = NULL;
-	printf("NB Cmd: %d\n", count_cmd(tokens));
+	args = (t_args *)malloc(sizeof(t_args));
+	if (!args)
+		return (NULL);
+	args->cmds = (t_cmd *)malloc(sizeof(t_cmd) * count_cmd(tokens));
+	if (!args->cmds)
+	{
+		free(args);
+		return (NULL);
+	}
+	free(tokens);
 	return (args);
 }
 
@@ -41,7 +49,9 @@ static int	count_cmd(char **tokens)
 		else
 		{
 			res++;
-			i++;
+			while (i < len && tokens[i][0] != '<' && tokens[i][0] != '>'
+				&& tokens[i][0] != '|')
+				i++;
 		}
 	}
 	return (res);
