d1aaa8a1c028755ae39ca5ee562c544586bea1ee
hchartie <chartierhugo098@gmail.com>
2026-05-04 15:45:55 +0200

    Add Utils_files

--- src/open_file.c
+++ src/open_file.c
@@ -6,7 +6,7 @@
 /*   By: hchartie <hchartie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/25 22:34:42 by hchartie          #+#    #+#             */
-/*   Updated: 2026/04/25 22:59:35 by hchartie         ###   ########.fr       */
+/*   Updated: 2026/05/04 15:44:24 by hchartie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	open_files(t_args *args)
 {
-	int	i;
+	int		i;
+	t_file	*current;
 
 	i = 0;
 	while (args->files[i])
 	{
+		current = args->files[i];
+		(void)current;
 		i++;
 	}
 }
